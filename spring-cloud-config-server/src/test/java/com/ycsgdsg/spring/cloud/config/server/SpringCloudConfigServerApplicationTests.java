677ed2ffb073191f3098e3dbb4a23defd877606c
Chao Yuan <ChaoYuan@ea.com>
2019-12-12 13:49:41 +0800

    add spring config server

---
+++ spring-cloud-config-server/src/test/java/com/ycsgdsg/spring/cloud/config/server/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycsgdsg.spring.cloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
