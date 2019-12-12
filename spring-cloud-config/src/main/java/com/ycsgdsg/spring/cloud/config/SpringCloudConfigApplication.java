a3d12ffa9f920a847e3b6da24891f710dee143bc
Chao Yuan <ChaoYuan@ea.com>
2019-12-12 18:00:25 +0800

    rename config server

--- spring-cloud-config/src/main/java/com/ycsgdsg/spring/cloud/config/SpringCloudConfigApplication.java
+++ spring-cloud-config/src/main/java/com/ycsgdsg/spring/cloud/config/SpringCloudConfigApplication.java
@@ -1,4 +1,4 @@
-package com.ycsgdsg.spring.cloud.config.server;
+package com.ycsgdsg.spring.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class SpringCloudConfigServerApplication {
+public class SpringCloudConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+		SpringApplication.run(SpringCloudConfigApplication.class, args);
 	}
 
 }
--- spring-cloud-config/src/test/java/com/ycsgdsg/spring/cloud/config/SpringCloudConfigApplicationTests.java
+++ spring-cloud-config/src/test/java/com/ycsgdsg/spring/cloud/config/SpringCloudConfigApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ycsgdsg.spring.cloud.config.server;
+package com.ycsgdsg.spring.cloud.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class SpringCloudConfigServerApplicationTests {
+public class SpringCloudConfigApplicationTests {
 
 	@Test
 	void contextLoads() {
